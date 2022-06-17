92cf768d8b48c829812baa53651a233a38e50a7b
Eiswald <spry.tent8477@fastmail.com>
2022-06-17 15:27:26 -0400

    Cleaning up, fixing dependencies, new template
    Actually now customizing the page to resemble an NFT mint page, also learning about react and material ui in general

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,29 @@
+import { AppBar, Typography, Toolbar } from '@material-ui/core'
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 import React from 'react'
 
 export default function Create() {
   return (
     <div>
-      Create page
+      <Typography variant="h2">Potato Pals</Typography>
+      <Box sx={{ flexGrow: 1 }}>
+      <AppBar>
+        <Toolbar>
+          <Typography> Potato Pals </Typography>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      </Box>
     </div>
   )
 }
